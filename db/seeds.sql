32a3563de4478ccacb4e5f1f66efcce530d5186e
Shetia Lamar <butlers@savannahstate.edu>
2018-08-17 17:51:33 -0400

    finished burger app

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,9 +1,9 @@
 -- Seed Burgers into Database ... 
 INSERT INTO burgers(burger_name, devoured, date)
-VALUES ("Big Mac", false, "2016-11-05 04:20:00"),
-  ("Whooper", false, "2016-11-05 04:20:01"),
-  ("Cheeseburger", false, "2016-11-05 04:20:02"),
-  ("Baconator", false, "2016-11-05 16:20:00"),
-  ("Quarter Pounder", false, "2016-11-05 16:20:01"),
-  ("McChicken", false, "2016-11-05 04:20:20"),
-  ("Double Stack", true, "2016-11-05 16:20:16");
+VALUES ("Big Mac", false, "2018-08-15 04:20:00"),
+  ("Whooper", false, "2018-08-15 04:20:01"),
+  ("Cheeseburger", false, "2018-08-15 04:20:02"),
+  ("Baconator", false, "2018-08-16 16:20:00"),
+  ("Quarter Pounder", false, "2018-08-16 16:20:01"),
+  ("McChicken", false, "2018-08-17 04:20:20"),
+  ("Double Stack", true, "2018-08-17 16:20:16");
