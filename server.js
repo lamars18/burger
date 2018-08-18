11ba5980e98b1648f30e7ff621124e0499512260
Shetia C. Butler Lamar <36701865+lamars18@users.noreply.github.com>
2018-08-17 22:46:22 -0400

    Update server.js

--- server.js
+++ server.js
@@ -22,4 +22,6 @@ app.use('/', router);
 
 // Open Server
 var port = process.env.PORT || 3000;
-app.listen(port);
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+  });
